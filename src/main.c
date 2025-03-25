1c341ac569c0f227c39f1c7772c80dc44b8c0f56
Hugo Ganet <ganethugo@gmail.com>
2025-03-25 15:12:33 +0100

    Add .gitignore and remove obj/ directories

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hganet <hganet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 13:08:24 by hganet            #+#    #+#             */
-/*   Updated: 2025/03/24 16:18:05 by hganet           ###   ########.fr       */
+/*   Updated: 2025/03/25 14:55:35 by hganet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,46 @@
  *
  * @param stack Pointer to the top of the stack.
  */
-void print_stack(t_node *stack)
+// static void print_stack(t_node *stack)
+// {
+// 	while (stack)
+// 	{
+// 		ft_printf("%d\n", stack->value);
+// 		stack = stack->next;
+// 	}
+// }
+
+static int	get_chunk_count(int size)
 {
-	while (stack)
-	{
-		ft_printf("%d\n", stack->value);
-		stack = stack->next;
-	}
+	if (size <= 100)
+		return (4);
+	return (8);
 }
 
 int main(int ac, char **av)
 {
-	t_node *stack_a;
+	t_node	*stack_a;
+	t_node	*stack_b;
+	int 	size;
+	t_chunk *chunks;
+	int		chunk_count;
 
 	if (ac < 2)
 		return (0);
-	stack_a = parse_args(ac, av);
 
-	ft_printf("Original values:\n");
-	print_stack(stack_a);
+	stack_a = parse_args(ac, av);
+	stack_b = NULL;
 
 	normalize_stack(stack_a);
 
-	ft_printf("\nNormalized (indexed) values:\n");
-	print_stack(stack_a);
+	size = stack_size(stack_a);
+	chunk_count = get_chunk_count(size);
+	chunks = generate_chunks(size, chunk_count);
 
+	process_chunks(&stack_a, &stack_b, chunks, chunk_count);
+	push_back_to_a(&stack_a, &stack_b);
+
+	free(chunks);
 	free_stack(stack_a);
 	return (0);
 }
